13019f3142f9896851a61af398db710767ef67ab
oliveur83 <tomolivierpro@gmail.com>
2022-11-14 15:06:23 +0100

    ajout de fonction

--- code/index.php
+++ code/index.php
@@ -1,12 +1,20 @@
-<!doctype html>
-<html lang="fr">
+<?php
+?>
+<!DOCTYPE html>
+<html>
+
 <head>
-  <meta charset="utf-8">
-  <title>Titre de la page</title>
-  <link rel="stylesheet" href="style.css">
-  <script src="script.js"></script>
+
 </head>
+
 <body>
+    <form action="emploi_du_temp.php" method="post">
+        <input type="submit" value="emploi du temps ">
+    </form>
+    <form action="creation_cours.php" method="post">
+        <input type="submit" value="creation cours ">
+    </form>
 
 </body>
+
 </html>
