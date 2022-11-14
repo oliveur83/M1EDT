fa59bf19c2efac0d907792fffff8328e73d5e633
oliveur83 <tomolivierpro@gmail.com>
2022-11-14 15:01:26 +0100

    fomatage du code,ajout de fonction

---
+++ code/CSS/index.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+
+</head>
+
+<body>
+    <form action="emploi_du_temp.php" method="post">
+        <input type="submit" value="emploi du temps ">
+    </form>
+    <form action="creation_cours.php" method="post">
+        <input type="submit" value="creation cours ">
+    </form>
+
+</body>
+
+</html>
