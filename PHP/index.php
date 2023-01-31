8cc75a0c5d82ef22086370e07974a9c9478cf050
Jean <90519788+Jean2B@users.noreply.github.com>
2023-01-31 15:38:12 +0100

    Nettoyage des fichiers

---
+++ PHP/index.php
@@ -0,0 +1,27 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+<link rel="stylesheet" href="../CSS/style.css" media="screen" type="text/css" />
+</head>
+
+<body>
+<center>
+    <div id="container">
+    <form action="emploi_du_temp.php" method="post">
+        <input type="submit" value="emploi du temps ">
+    </form>
+    <form action="creation_cours.php" method="post">
+        <input type="submit" value="creation cours ">
+    </form>
+    <form action="connexion.php" method="post">
+        <input type="submit" value="connexion ">
+    </form>
+
+
+    </div>
+</body>
+
+</html>
